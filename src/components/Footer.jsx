5bef346ea9cac23e36d5f1d74708c13511ef333d
ahmed-abdat <ahmedeabdat@gmail.com>
2023-01-09 21:27:11 +0000

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,9 +24,9 @@ const Footer = ({ footerAPI: { titles, links } }) => {
               </ul>
             ))}
           </div>
-          <div className='mt-5 text-center'>
+          {/* <div className='mt-5 text-center'>
             <p className='text-sm md:text-center'>Copyright<sup className='text-base font-bold'>&copy;</sup> All Reserved Rights <span className='font-semibold'>JSSTACK DEVELOPERS {Year}</span></p>
-          </div>
+          </div> */}
         </div>
       </footer>
    </>
